6b53ba5c90e0abed1b2695af10fbe426cf191f5f
nfan17 <nicolasfan51@gmail.com>
2024-09-08 13:14:13 -0700

    nf-i2c-cli: f7 support, i2c cli lib

--- app/cli_f7/main.c
+++ app/cli_f7/main.c
@@ -4,32 +4,41 @@
 
 #include "bsp.h"
 
+#include "i2c.h"
+#include "i2c_access.h"
 #include "usart.h"
 #include "usart_cli.h"
 
-/* The task functions. */
-void vTask1( void * pvParameters );
+#include "tmp102.h"
 
-/*-----------------------------------------------------------*/
-
-StackType_t task1_stack[50];
-StaticTask_t task1_buffer;
+#include "string_hex.h"
 
 /*-----------------------------------------------------------*/
 
 void blink(int argc, char* argv[]);
+void read_temp(int argc, char* argv[]);
 
 Usart usart;
+I2c i2c;
+
+Tmp102 tmp;
 
 int main(void)
 {
 
-    BSP_Init(&usart);
+    BSP_Init(&usart, &i2c);
 
-    Command commands[2] = { 
-        {"Blink", blink, "Blinks LED."}
+    Tmp102_Init(&tmp, &i2c, TMP102_ADDR_GND);
+
+    init_i2c_access(&i2c);
+
+    Command commands[4] = { 
+        {"Blink", blink, "Blinks LED."},
+        {"Temp", read_temp, "Reads temperature."},
+        {"IWrite", write_i2c, "Writes I2c."},
+        {"IRead", read_i2c, "Reads I2c."}
     };
-    create_cli_task(&usart, commands, 1);
+    create_cli_task(&usart, commands, 4);
 
     /* Start the scheduler to start the tasks executing. */
     vTaskStartScheduler();
@@ -41,25 +50,14 @@ int main(void)
     return 0;
 }
 
-
-void vTask1( void * pvParameters )
-{
-
-    for ( ; ; )
-    {
-
-        /* Delay for a period. */
-        vTaskDelay(10);
-    }
-}
-
 void blink(int argc, char* argv[])
 {
     GPIOB->ODR ^= GPIO_ODR_OD0;
     cli_write("Blink - %d", GPIOB->ODR & GPIO_ODR_OD0);
 }
 
-void USART3_IRQHandler(void)
+void read_temp(int argc, char* argv[])
 {
-    usart_rx_callback();
+    float x = tmp.get_temp_c(&tmp);
+    cli_write("Temp: %dC", (int) x);
 }
