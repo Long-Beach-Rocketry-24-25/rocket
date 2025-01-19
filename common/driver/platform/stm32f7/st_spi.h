93f7c42d7e2673f9872f424b66c81d861521e4e9
nfan17 <nicolasfan51@gmail.com>
2025-01-19 14:39:41 -0800

    working flash driver

--- common/driver/platform/stm32f7/st_spi.h
+++ common/driver/platform/stm32f7/st_spi.h
@@ -19,4 +19,6 @@ typedef struct
 
 void StSpiInit(Spi *spi, StPrivSpi *st_spi, uint32_t base_addr, Timeout *timer);
 void StSpiConfig(Spi * spi);
+bool StSpiSend(Spi * spi, uint8_t *data, size_t size);
+bool StSpiRead(Spi * spi, uint8_t *data, size_t size);
 bool StSpiTransact(Spi * spi, uint8_t *txdata, uint8_t *rxdata, size_t size);
