ad3cdb06f0f0749991005bc5634ccdd04385deec
nfan17 <nicolasfan51@gmail.com>
2025-03-12 22:22:45 -0700

    s

--- app/cli_app/BSP_F746/cli_app_bsp.c
+++ app/cli_app/BSP_F746/cli_app_bsp.c
@@ -3,9 +3,6 @@
 
 #define EXIT_IF_FAIL(cond) EXIT_IF(!(cond), false)
 
-// Sequential use of these, so using one is fine. Not thread safe.
-static Timeout time;
-static FrtTimerData frt;
 static Mem memory;
 static uint8_t driver_mem[DRIVER_MEM_SIZE] = {0};
 
@@ -13,15 +10,15 @@ bool BSP_Init(Usart* usart, I2c* temp_i2c, Gpio* led_gpio)
 {
     EXIT_IF_FAIL(InitStalloc(&memory, driver_mem, DRIVER_MEM_SIZE));
 
+    // Single FRT timer.
+    Timeout* time = make_frt_timer(&memory, 100);
+
     // LED GPIO
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 
     EXIT_IF_FAIL(
         MakeStGpio(led_gpio, &memory,
                    (StGpioParams){{0}, GPIOB_BASE, 0, {1, 0, 0, 0, 0}}));
-
-    // Single FreeRTOS timer
-    frt_timer_init(&time, &frt, 100);
 
     // USART3
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
@@ -33,7 +30,7 @@ bool BSP_Init(Usart* usart, I2c* temp_i2c, Gpio* led_gpio)
 
     // PD8/9 AF 7
     EXIT_IF_FAIL(MakeStUsart(
-        usart, &memory, USART3_BASE, SystemCoreClock, 115200, &time,
+        usart, &memory, USART3_BASE, SystemCoreClock, 115200, time,
         (StGpioParams){{0}, GPIOD_BASE, 8, {ALT_FUNC, 0, 0, 0, 0x7}},
         (StGpioParams){{0}, GPIOD_BASE, 9, {ALT_FUNC, 0, 0, 0, 0x7}}));
 
@@ -43,7 +40,7 @@ bool BSP_Init(Usart* usart, I2c* temp_i2c, Gpio* led_gpio)
 
     // PB8/9 AF 4
     const StGpioSettings i2c_conf = {ALT_FUNC, OPEN_DRAIN, 0, PULL_UP, 0x4};
-    EXIT_IF_FAIL(MakeStI2c(temp_i2c, &memory, I2C1_BASE, 0x20B, &time,
+    EXIT_IF_FAIL(MakeStI2c(temp_i2c, &memory, I2C1_BASE, 0x20B, time,
                            (StGpioParams){{0}, GPIOB_BASE, 8, i2c_conf},
                            (StGpioParams){{0}, GPIOB_BASE, 9, i2c_conf}));
 
