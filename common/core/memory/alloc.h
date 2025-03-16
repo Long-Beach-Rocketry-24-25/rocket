71b65e043d6f7bd1931e00d61023e68d9632b202
nfan17 <nicolasfan51@gmail.com>
2025-03-16 15:28:05 -0700

    buf alloc

--- common/core/memory/alloc.h
+++ common/core/memory/alloc.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #define ALLOC(mem, type) (type*)(mem)->alloc(sizeof(type))
+#define ALLOC_N(mem, type, num) (type*)(mem)->alloc(sizeof(type) * num)
 
 typedef struct Mem Mem;
 
