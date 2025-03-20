856b802f8188b58f93146fb1e7463757fc72c9e8
ayleenp27 <ayleenperez004@gmail.com>
2025-03-20 16:16:25 -0700

    removed gpio test and fixed c make error

--- common/core/periph/switches/io_input.h
+++ common/core/periph/switches/io_input.h
@@ -3,27 +3,27 @@
  * 
  */
 
- #pragma once
+#pragma once
 
- #include <stdbool.h>
- #include <stddef.h>
- #include <stdint.h>
- 
- //typedef struct Usart Usart;
- 
- //struct Usart
- //{
- //    bool (*send)(Usart* usart, const uint8_t* data, size_t size);
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+//typedef struct Usart Usart;
+
+//struct Usart
+//{
+//    bool (*send)(Usart* usart, const uint8_t* data, size_t size);
 //     bool (*recv)(Usart* usart, uint8_t* data, size_t size);
- //
- //    void* priv;
- //};
+//
+//    void* priv;
+//};
 
 typedef struct IoInput IoInput;
 
 struct IoInput
 {
     // return true if io is 1 or or false if 0
-    bool (*get_state)(IoInput *in);
-    void *priv;
-}
+    bool (*get_state)(IoInput* in);
+    void* priv;
+};
