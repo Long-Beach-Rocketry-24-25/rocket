4344a020103a1de5c9f09fa8c03b6bb01b2199fd
ayleenp27 <ayleenperez004@gmail.com>
2025-03-20 16:23:25 -0700

    formated

--- common/core/periph/switches/count_debounce.c
+++ common/core/periph/switches/count_debounce.c
@@ -5,35 +5,35 @@
 //initialization
 void CountDebounce_Init(CountDebounce* db, IoInput* input, uint8_t threshold)
 {
-    db->state = DEBOUNCE_OFF; // its gonna start at released
+    db->state = DEBOUNCE_OFF;  // its gonna start at released
     db->counter = 0;
     db->threshold = threshold;
 
     input->get_state = CountDebounce_GetStateFromIoInput;
-    input->priv = (void*) db;
+    input->priv = (void*)db;
 }
 
 // updating the logic lowk gonna call this a lot IMPORTANT
 void CountDebounce_Update(CountDebounce* db, IoInput* input)
 {
-    bool raw_state = input->get_state(input); // reads the input here
+    bool raw_state = input->get_state(input);  // reads the input here
 
     // if the "raw" state is different from stable its gonna count
-    if ((raw_state && db->state == DEBOUNCE_OFF) || 
-        (!raw_state && db->state == DEBOUNCE_ON)) 
+    if ((raw_state && db->state == DEBOUNCE_OFF) ||
+        (!raw_state && db->state == DEBOUNCE_ON))
     {
         db->counter++;
 
         // when counter exceeds the threshold then we have to update the state within
-        if (db->counter >= db->threshold) 
+        if (db->counter >= db->threshold)
         {
             db->state = raw_state ? DEBOUNCE_ON : DEBOUNCE_OFF;
             db->counter = 0;  //resets counter after it switches it's state
         }
-    } 
-    else 
+    }
+    else
     {
-        db->counter = 0;  // counter resettt 
+        db->counter = 0;  // counter resettt
     }
 }
 
@@ -42,7 +42,7 @@ void CountDebounce_Update(CountDebounce* db, IoInput* input)
 //have to call CountDebounce_Update() regularly to stay current
 bool CountDebounce_GetStateFromIoInput(IoInput* input)
 {
-    CountDebounce* db = (CountDebounce*) input->priv;
+    CountDebounce* db = (CountDebounce*)input->priv;
     return db->state == DEBOUNCE_ON;
 }
 
--- common/core/periph/switches/count_debounce.h
+++ common/core/periph/switches/count_debounce.h
@@ -1,35 +1,34 @@
 #pragma once
 
-#include "io_input.h"
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include "io_input.h"
 
 //will define actual debouncing logic to use in c file
 
 //two debounce states, on and off
 
-typedef enum {
+typedef enum
+{
     DEBOUNCE_OFF = 0,  // button when released
-    DEBOUNCE_ON = 1       // button when pressed
+    DEBOUNCE_ON = 1    // button when pressed
 } DebounceState;
 
-
-typedef struct {
-    DebounceState state;   //stable state 
-    uint8_t counter;      //count opposite readings 
+typedef struct
+{
+    DebounceState state;  //stable state
+    uint8_t counter;      //count opposite readings
     //if n opposite values in a row - > change the state
     //else stay in current state and reset counter
-    uint8_t threshold;   //switch states
+    uint8_t threshold;  //switch states
 } CountDebounce;
 
-
-
-//initiate debounce 
+//initiate debounce
 //db is pointer to debouncer
 // threshold is # of opposite reads before changing state
 void CountDebounce_Init(CountDebounce* db, IoInput* input, uint8_t threshold);
 
-//to update 
+//to update
 void CountDebounce_Update(CountDebounce* db, IoInput* input);
 
 //on or off? hmmm this will tell us
