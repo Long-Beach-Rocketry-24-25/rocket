2e9057c744e16e35b4506ba530205489abd7e57c
TJMalaska <malaskatj@gmail.com>
2025-04-04 18:30:07 -0700

    added submodule for nanopb

--- common/app/freertos/lbr_net/uart_lbr_net.c
+++ common/app/freertos/lbr_net/uart_lbr_net.c
@@ -1,13 +1,65 @@
 #include "uart_lbr_net.h"
 
-void uart_lbr_net_init(uart_lbr_net* uart_lbr_net, Send* sender,
-                       uint8_t address)
+static Bus bus;
+static Usart* comm;
+static SemaphoreHandle_t task_semaphore = NULL;
+
+bool send(uint8_t* buff, size_t size)
 {
-    Bus bus;
+    if (size > MAX_RECEIVE_BUF_SIZE - CHECKSUM_SIZE - START_BYTE_SIZE)
+    {
+        return false;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        comm->send(&comm, &buff, size);
+    }
+    return true;
+}
+
+void lbr_net_process_task()
+{
+    const TickType_t max_block_time = pdMS_TO_TICKS(UINT32_MAX);
+    task_semaphore = xSemaphoreCreateBinary();
+    while (1)
+    {
+        if (xSemaphoreTake(task_semaphore, max_block_time) == pdTRUE)
+        {
+        }
+    }
+}
+
+void usart_rx_callback()
+{
+    uint8_t data = 0;
+    if (comm->recv(comm, &data, 1))
+    {
+        BaseType_t higher_prio_task_woken = pdFALSE;
+        bus.read_byte(&bus, bus.receive_buffer);
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        if (bus.state = FINISHED)
+        {
+            if (task_semaphore == NULL)
+            {
+                return;
+            }
+            else
+            {
+                xSemaphoreGiveFromISR(task_semaphore)
+            }
+        }
+        portYIELD_FROM_ISR(higher_prio_task_woken);
+    }
+}
+
+bool receive(uint8_t* buff, size_t size)
+{
+}
+
+void uart_lbr_init(uint8_t address, net_if* net_if, Usart* usart)
+{
     lbr_net_node_init(&bus, address);
-    uart_lbr_net->bus = bus;
-    uart_lbr_net->send_data = sender->write_str;
-    //send
-    //reicves
-    //calback
+    comm = usart;
+    net_if->receive = &receive;
+    net_if->send = &send;
 }
--- common/app/freertos/lbr_net/uart_lbr_net.h
+++ common/app/freertos/lbr_net/uart_lbr_net.h
@@ -1,12 +1,22 @@
+/*
+notes from nick
+the interupt will pass a complete message in and notify that previously blocked state updater should be ready
+The main loop should just call funcitons based off of the strcut of states
+*/
 #pragma once
 
 #include "lbr_net.h"
 #include "net_interface.h"
 
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+
 #include "retry_timer.h"
-#include "ring_buffer.h"
 #include "usart.h"
 
 void usart_rx_callback();
 
-void uart_lbr_init(uint8_t address, net_if net_if);
+void uart_lbr_init(uint8_t address, net_if* net_if, Usart* usart);
+
+void lbr_net_process_task();
