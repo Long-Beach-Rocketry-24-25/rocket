a89366cbc181715faf46bf307e07b9aa9b3888f4
nfan17 <nicolasfan51@gmail.com>
2025-04-05 21:30:11 -0700

    save

---
+++ tools/data/log_splitter.py
@@ -0,0 +1,17 @@
+
+import sys
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 2:
+        print("Please provide a file name in the first command line argument.")
+    else:
+
+        fname = sys.argv[1]
+
+        with open(fname, "r") as r:
+            data = [line for group in r.readlines() for line in group.split("|") if line.strip()]
+
+        # print(data)
+        for line in data:
+            print(line)
