e557b197c19bb63434cdbf98637b9afcaf5c0b78
ayleenp27 <ayleenperez004@gmail.com>
2025-04-15 16:43:46 -0700

    Implemented CountDebounce with IoInput interface and GTest coverage

--- app/gpio_test/BSP_F401/gpio_test_bsp.h
+++ app/gpio_test/BSP_F401/gpio_test_bsp.h
@@ -6,27 +6,3 @@
 
 #include <stdint.h>
 #include "st_gpio.h"
-
-void BSP_Init(Gpio* led_gpio);
-
-typedef enum
-{
-    BUTTON_RELEASED = 0,
-    BUTTON_PRESSED
-} DebounceState;
-
-typedef struct
-{
-    Gpio gpio;
-    DebounceState state;
-    uint8_t raw_state;
-    uint32_t last_change_ms;
-    uint32_t debounce_time_ms;
-} DebounceButton_t;
-
-void Debounce_Init(DebounceButton_t* btn, StGpioParams* gpio_params,
-                   uint32_t debounce_time_ms);
-void Debounce_Update(DebounceButton_t* btn, uint32_t now_ms);
-uint8_t Debounce_IsPressed(DebounceButton_t* btn);
-uint8_t Debounce_IsReleased(DebounceButton_t* btn);
-DebounceState Debounce_GetState(DebounceButton_t* btn);
--- common/core/switches/count_debounce.c
+++ common/core/switches/count_debounce.c
@@ -1,12 +1,9 @@
 #include "count_debounce.h"
-//#include "core_cm4.h" // SysTick
-#include <stdint.h>
 
-//initialization
 void CountDebounce_Init(CountDebounce* db, IoInput* input, Gpio* gpio,
                         uint8_t threshold)
 {
-    db->state = DEBOUNCE_OFF;  // its gonna start at released
+    db->state = DEBOUNCE_OFF;
     db->counter = 0;
     db->threshold = threshold;
     db->gpio = gpio;
@@ -15,38 +12,11 @@ void CountDebounce_Init(CountDebounce* db, IoInput* input, Gpio* gpio,
     input->priv = (void*)db;
 }
 
-//
-//void CountDebounce_Update(CountDebounce* db, IoInput* input)
-//{
-//    bool raw_state = input->get_state(input);  // reads the input here
-//
-//    // if the "raw" state is different from stable its gonna count
-//    if ((raw_state && db->state == DEBOUNCE_OFF) ||
-//        (!raw_state && db->state == DEBOUNCE_ON))
-//    {
-//        db->counter++;
-//
-//        // when counter exceeds the threshold then we have to update the state within
-//        if (db->counter >= db->threshold)
-//        {
-//            db->state = raw_state ? DEBOUNCE_ON : DEBOUNCE_OFF;
-//            db->counter = 0;  //resets counter after it switches it's state
-//        }
-//    }
-//    else
-//    {
-//        db->counter = 0;  // counter resettt
-//    }
-//}
-
 bool CountDebounce_GetStateFromIoInput(IoInput* input)
 {
     CountDebounce* db = (CountDebounce*)input->priv;
-    //updated logic here
-    //read raw input from gpio
     bool raw = db->gpio->read(db->gpio);
 
-    //if input different from current stable state, start counting
     if ((raw && db->state == DEBOUNCE_OFF) ||
         (!raw && db->state == DEBOUNCE_ON))
     {
@@ -69,13 +39,11 @@ DebounceState CountDebounce_GetState(CountDebounce* db)
     return db->state;
 }
 
-//true is button is pressed and stable
 bool CountDebounce_IsPressed(CountDebounce* db)
 {
     return db->state == DEBOUNCE_ON;
 }
 
-//true if button is released at stable
 bool CountDebounce_IsReleased(CountDebounce* db)
 {
     return db->state == DEBOUNCE_OFF;
