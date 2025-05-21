951825e5f91aa942a7eaa6a8b681ce2029ff6695
Nick Fan <nicolasfan51@gmail.com>
2025-05-21 15:23:02 -0700

    save for reference

---
+++ app/flight_computer/main.c
@@ -0,0 +1,38 @@
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "flight_computer_bsp.h"
+
+#include "gpio.h"
+#include "i2c.h"
+#include "usart.h"
+
+#include "debug_app.h"
+
+/*-----------------------------------------------------------*/
+
+Usart usart;
+I2c i2c;
+Gpio red_led;
+Gpio green_led;
+Gpio blue_led;
+
+int main(void)
+{
+
+    BSP_Init(&usart, &i2c, &red_led, &green_led, &blue_led);
+
+    red_led.set(&red_led, true);
+    green_led.set(&green_led, true);
+    blue_led.set(&blue_led, true);
+
+    DebugAppCreate(&usart, &i2c, &red_led);
+
+    /* Start the scheduler to start the tasks executing. */
+    vTaskStartScheduler();
+
+    while (1);
+
+    return 0;
+}
